e00044eec273eb9b9446f45add2513ac54e475da
isuryanarayanan <devdoodan@gmail.com>
2021-12-20 07:11:00 +0530

    hosting the site

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,13 @@ import store from "./store/index";
 import App from "./App.vue";
 import gAuthPlugin from "vue3-google-oauth2";
 
+var development =
+  "1027589441319-e1vie4dkmdpdit6q8ps3tqpqo0dafj52.apps.googleusercontent.com";
+var deployment =
+  "1027589441319-e1vie4dkmdpdit6q8ps3tqpqo0dafj52.apps.googleusercontent.com";
+
 const gAuthOptions = {
-  clientId:
-    "1027589441319-hc37u1500vd6p3engsq9pgcllcstfs4l.apps.googleusercontent.com",
+  clientId: deployment,
   scope: "profile email openid",
   prompt: "consent",
   fetch_basic_profile: false,
